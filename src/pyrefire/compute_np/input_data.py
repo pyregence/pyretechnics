0b14de8e13ed6d5d85e8a5423ff58cb6eee48401
Gary Johnson <lambdatronic@disroot.org>
2023-12-12 11:19:00 -0500

    Adding a basic README.md, org/PyreFire.org, and some Cython experiments under src/

---
+++ src/pyrefire/compute_np/input_data.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+# 2D Matrices
+array_1 = np.random.uniform(0, 1000, size=(3000, 2000)).astype(np.intc)
+array_2 = np.random.uniform(0, 1000, size=(3000, 2000)).astype(np.intc)
+
+# Scalars
+a = 4
+b = 3
+c = 9
---
+++ src/pyrefire/first_test/runner.py
@@ -0,0 +1,4 @@
+from test_cython import integrate_f
+
+print("Integrating x^2 - x from 0 to 100...")
+print(integrate_f(0, 100, 100))
