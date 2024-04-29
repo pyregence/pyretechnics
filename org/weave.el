7869d102453adcfed919e06b9ec1c7ece699299a
Gary Johnson <lambdatronic@disroot.org>
2024-04-29 18:18:54 -0400

    Update weave.el for org->html weaving

--- org/weave.el
+++ org/weave.el
@@ -1,50 +1,25 @@
 #!/usr/bin/env -S emacs -Q --script
 
 ;;==========================================================
-;; Configure org's PDF export
+;; Configure org's HTML export
 ;;==========================================================
 
 (require 'org)
+(require 'ox-html)
+(require 'htmlize)
+(require 'python)
 
-(setq org-latex-classes                '(("article" "\\documentclass[11pt]{article}"
-                                          ("\\section{%s}"       . "\\section*{%s}")
-                                          ("\\subsection{%s}"    . "\\subsection*{%s}")
-                                          ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
-                                          ("\\paragraph{%s}"     . "\\paragraph*{%s}")
-                                          ("\\subparagraph{%s}"  . "\\subparagraph*{%s}")))
-      org-export-latex-hyperref-format "\\ref{%s}"
-      org-latex-default-packages-alist '(("AUTO"     "inputenc"  t)
-                                         (""         "graphicx"  t)
-                                         (""         "longtable" nil)
-                                         (""         "float"     nil)
-                                         (""         "wrapfig"   nil)
-                                         (""         "rotating"  nil)
-                                         ("normalem" "ulem"      t)
-                                         (""         "amsmath"   t)
-                                         (""         "textcomp"  t)
-                                         (""         "marvosym"  t)
-                                         (""         "wasysym"   t)
-                                         (""         "amssymb"   t)
-                                         (""         "minted"    nil)
-                                         (""         "hyperref"  nil)
-                                         "\\tolerance=1000")
-      org-latex-listings               'minted
-      org-latex-minted-options         '(("linenos"         "false")
-                                         ("frame"           "single")
-                                         ("fontsize"        "\\scriptsize")
-                                         ("baselinestretch" "1.0"))
-      org-latex-pdf-process            '("pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"
-                                         "bibtex %b"
-                                         "pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"
-                                         "pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f")
-      make-backup-files                nil)
+;; TODO: Add source code highlighting settings from my init.el
+;; (setq org-src-fontify-natively t)
+
+(setq python-indent-guess-indent-offset nil)
 
 ;;==========================================================
-;; Now load Pyretechnics.org and weave its prose into a PDF
+;; Now load Pyretechnics.org and weave it into an HTML file
 ;;==========================================================
 
 (find-file "Pyretechnics.org")
-(org-latex-export-to-pdf)
+(org-html-export-to-html)
 
 ;;==========================================================
 ;; Obligatory calling shell protection
