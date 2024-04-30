708b9ebf4f9a2ef9039f81b7a1f5037590021097
Gary Johnson <lambdatronic@disroot.org>
2024-04-30 12:18:09 -0400

    Simplified detangle.el to search the repository root directory by default

--- org/detangle.el
+++ org/detangle.el
@@ -1,40 +1,33 @@
 #!/usr/bin/env -S emacs -Q --script
 
 ;;==========================================================
-;; Load all files under the specified root-directory
+;; Load all source code files within the repository
 ;; and detangle each one back into pyretechnics.org
 ;;==========================================================
 
 (require 'org)
 
 ;; Prevent extra whitespace from being injected into code blocks
-(setq org-src-preserve-indentation t)
+(setq org-src-preserve-indentation      t
+      python-indent-guess-indent-offset nil)
 
-(defun valid-directory-p (directory-name)
-  (and directory-name (file-directory-p directory-name)))
-
-(defvar detangle-file-regexp (string-join '("\\.txt$"
-                                            "\\.org$"
-                                            "\\.json$"
-                                            "\\.el$"
+(let* ((script-directory     (file-name-directory load-file-name))
+       (root-directory       (file-name-parent-directory script-directory))
+       (detangle-file-regexp (string-join '("\\.py$"
+                                            "\\.pyx$"
                                             "\\.sh$"
-                                            "\\.py$"
-                                            "\\.pyx$")
+                                            "\\.el$")
                                           "\\|"))
-
-(let ((root-directory (car argv)))
-  (if (not (valid-directory-p root-directory))
-      (princ "Usage: detangle.el <source-directory>\n")
-    (let ((script-directory  (file-name-directory load-file-name))
-          (files-to-detangle (seq-remove (lambda (file-name) (string-match-p "/target/\\|/classes/" file-name))
+       (files-to-detangle    (seq-remove (lambda (file-name) (string-match-p "/org/\\|/doc/" file-name))
                                          (directory-files-recursively root-directory detangle-file-regexp))))
-      ;; Enter the org/ directory and set the current buffer to pyretechnics.org so that (save-buffer) works later
-      (cd script-directory)
-      (find-file "./pyretechnics.org")
-      (mapc (lambda (source-file)
-              (org-babel-detangle source-file)
-              (save-buffer)) ; Save pyretechnics.org after each detangling operation
-            files-to-detangle))))
+  ;; Enter the org/ directory and set the current buffer to pyretechnics.org so that (save-buffer) works later
+  (cd script-directory)
+  (find-file "./pyretechnics.org")
+  (mapc (lambda (source-file)
+          (message "Detangling %s" source-file)
+          (org-babel-detangle source-file)
+          (save-buffer)) ; Save pyretechnics.org after each detangling operation
+        files-to-detangle))
 
 ;;==========================================================
 ;; Obligatory calling shell protection
