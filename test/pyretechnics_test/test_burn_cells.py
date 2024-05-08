a78fba1a0abf5b9a32a365a0ef6b70072ae5f09d
Gary Johnson <lambdatronic@disroot.org>
2024-05-08 11:05:06 -0400

    Added get_project_root() to pyretechnics_test.test_burn_cells so that the test GeoTIFFs can be found from any start point

--- test/pyretechnics_test/test_burn_cells.py
+++ test/pyretechnics_test/test_burn_cells.py
@@ -1,15 +1,32 @@
 # [[file:../../org/pyretechnics.org::add-landfire-layers-to-pyretechnics-inputs][add-landfire-layers-to-pyretechnics-inputs]]
+import os
 from pyretechnics.load_landfire import read_landfire_rasters_as_pyretechnics_inputs
 
+def get_project_root(current_dir=os.curdir):
+    """
+    Search up the directory tree from current_dir until we find a directory containing guix.scm,
+    and return this directory path. If the filesystem root directory (/) is reached, return None.
+    """
+    if current_dir == "/":
+        return None
+    elif os.path.exists(current_dir + "/guix.scm"):
+        return current_dir
+    else:
+        return get_project_root(os.path.dirname(os.path.abspath(current_dir)))
+
+
+project_root = get_project_root()
+
+
 landfire_file_paths = {
-    "elevation"          : "test/data/LF2020_Elev_220_CONUS/LC20_Elev_220.tif",
-    "slope"              : "test/data/LF2020_SlpP_220_CONUS/LC20_SlpP_220.tif",
-    "aspect"             : "test/data/LF2020_Asp_220_CONUS/LC20_Asp_220.tif",
-    "fuel_model"         : "test/data/LF2022_FBFM40_230_CONUS/LC22_F40_230.tif",
-    "canopy_cover"       : "test/data/LF2022_CC_230_CONUS/LC22_CC_230.tif",
-    "canopy_height"      : "test/data/LF2022_CH_230_CONUS/LC22_CH_230.tif",
-    "canopy_base_height" : "test/data/LF2022_CBH_230_CONUS/LC22_CBH_230.tif",
-    "canopy_bulk_density": "test/data/LF2022_CBD_230_CONUS/LC22_CBD_230.tif",
+    "elevation"          : project_root + "/test/data/LF2020_Elev_220_CONUS/LC20_Elev_220.tif",
+    "slope"              : project_root + "/test/data/LF2020_SlpP_220_CONUS/LC20_SlpP_220.tif",
+    "aspect"             : project_root + "/test/data/LF2020_Asp_220_CONUS/LC20_Asp_220.tif",
+    "fuel_model"         : project_root + "/test/data/LF2022_FBFM40_230_CONUS/LC22_F40_230.tif",
+    "canopy_cover"       : project_root + "/test/data/LF2022_CC_230_CONUS/LC22_CC_230.tif",
+    "canopy_height"      : project_root + "/test/data/LF2022_CH_230_CONUS/LC22_CH_230.tif",
+    "canopy_base_height" : project_root + "/test/data/LF2022_CBH_230_CONUS/LC22_CBH_230.tif",
+    "canopy_bulk_density": project_root + "/test/data/LF2022_CBD_230_CONUS/LC22_CBD_230.tif",
 }
 
 
@@ -56,4 +73,5 @@ def test_burn_one_cell():
         'fire_type'              : 1,
         'eccentricity'           : 0.5583790663230914,
     }
+    return result
 # test-burn-cells-on-pyretechnics-inputs ends here
