1b9310ba5fb988f8afe1a911d5fe5758c110f170
Gary Johnson <lambdatronic@disroot.org>
2024-08-12 16:25:37 -0400

    Simplified API for pyretechnics.vector_utils.rotate_on_sloped_plane

--- src/pyretechnics/vector_utils.py
+++ src/pyretechnics/vector_utils.py
@@ -1,6 +1,7 @@
 # [[file:../../org/pyretechnics.org::vector-utilities][vector-utilities]]
 from math import degrees, radians, sin, cos, acos, atan2, sqrt
 import numpy as np
+import pyretechnics.conversion as conv
 
 
 def vector_magnitude(vector):
@@ -11,11 +12,11 @@ def as_unit_vector(vector):
     return np.asarray(vector) / vector_magnitude(vector)
 
 
-def to_slope_plane(vector_2d, upslope_vector_2d):
+def to_slope_plane(vector_2d, elevation_gradient):
     return (
         vector_2d[0],
         vector_2d[1],
-        np.dot(vector_2d, upslope_vector_2d)
+        np.dot(vector_2d, elevation_gradient)
     )
 
 
@@ -23,8 +24,8 @@ def to_horizontal_plane(vector_3d):
     return vector_3d[0:2]
 
 
-def get_slope_normal_vector(upslope_vector_2d):
-    slope_normal_vector = (-upslope_vector_2d[0], -upslope_vector_2d[1], 1)
+def get_slope_normal_vector(elevation_gradient):
+    slope_normal_vector = (-elevation_gradient[0], -elevation_gradient[1], 1)
     return as_unit_vector(slope_normal_vector)
 
 
@@ -55,10 +56,13 @@ def rotate_z(vector, theta):
     return np.dot(rotation_matrix, vector)
 
 
-def rotate_on_sloped_plane(vector, theta, slope_normal_vector):
-    """Rotate a 3D vector <x,y,z> theta degrees clockwise on the plane defined by the slope_normal_vector."""
+def rotate_on_sloped_plane(vector, theta, slope, aspect):
+    """Rotate a 3D vector <x,y,z> theta degrees clockwise on the plane defined by the slope and aspect."""
+    # Calculate the slope normal vector from the slope and aspect
+    elevation_gradient  = conv.azimuthal_to_cartesian(slope, conv.opposite_direction(aspect))
+    slope_normal_vector = get_slope_normal_vector(elevation_gradient)
+    (n_Sx, n_Sy, n_Sz)  = slope_normal_vector
     # Rotate around z axis to align n_S with the y-z plane
-    (n_Sx, n_Sy, n_Sz) = slope_normal_vector
     z_angle = degrees(atan2(n_Sx, n_Sy))
     vector1 = rotate_z(vector, z_angle)
     # Rotate around x axis to align n_S with the z axis
