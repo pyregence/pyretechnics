3f09bb0df8cacdb0392dc3e5d35699418b7d5862
Gary Johnson <lambdatronic@disroot.org>
2024-08-13 12:22:08 -0400

    Simplified pyretechnics.vector_utils.rotate_on_slope_plane

--- src/pyretechnics/vector_utils.py
+++ src/pyretechnics/vector_utils.py
@@ -1,5 +1,5 @@
 # [[file:../../org/pyretechnics.org::vector-utilities][vector-utilities]]
-from math import degrees, radians, sin, cos, acos, atan2, sqrt
+from math import radians, sin, cos, sqrt
 import numpy as np
 import pyretechnics.conversion as conv
 
@@ -29,51 +29,12 @@ def get_slope_normal_vector(elevation_gradient):
     return as_unit_vector(slope_normal_vector)
 
 
-def rotate_x(vector, theta):
-    """Rotate a 3D vector <x,y,z> theta degrees clockwise around the x axis."""
-    theta_rad       = radians(theta)
-    rotation_matrix = [[1.0,            0.0,             0.0],
-                       [0.0, cos(theta_rad), -sin(theta_rad)],
-                       [0.0, sin(theta_rad),  cos(theta_rad)]]
-    return np.dot(rotation_matrix, vector)
-
-
-def rotate_y(vector, theta):
-    """Rotate a 3D vector <x,y,z> theta degrees clockwise around the y axis."""
-    theta_rad       = radians(theta)
-    rotation_matrix = [[ cos(theta_rad), 0.0, sin(theta_rad)],
-                       [            0.0, 1.0,            0.0],
-                       [-sin(theta_rad), 0.0, cos(theta_rad)]]
-    return np.dot(rotation_matrix, vector)
-
-
-def rotate_z(vector, theta):
-    """Rotate a 3D vector <x,y,z> theta degrees clockwise around the z axis."""
-    theta_rad       = radians(theta)
-    rotation_matrix = [[cos(theta_rad), -sin(theta_rad), 0.0],
-                       [sin(theta_rad),  cos(theta_rad), 0.0],
-                       [           0.0,             0.0, 1.0]]
-    return np.dot(rotation_matrix, vector)
-
-
 def rotate_on_sloped_plane(vector, theta, slope, aspect):
     """Rotate a 3D vector <x,y,z> theta degrees clockwise on the plane defined by the slope and aspect."""
     # Calculate the slope normal vector from the slope and aspect
     elevation_gradient  = conv.azimuthal_to_cartesian(slope, conv.opposite_direction(aspect))
     slope_normal_vector = get_slope_normal_vector(elevation_gradient)
-    (n_Sx, n_Sy, n_Sz)  = slope_normal_vector
-    # Rotate around z axis to align n_S with the y-z plane
-    z_angle = degrees(atan2(n_Sx, n_Sy))
-    vector1 = rotate_z(vector, z_angle)
-    # Rotate around x axis to align n_S with the z axis
-    x_angle = degrees(acos(n_Sz / vector_magnitude(slope_normal_vector)))
-    vector2 = rotate_x(vector1, x_angle)
-    # Now the sloped plane is the x-y plane!
-    # Rotate around z axis (a.k.a. n_S) to turn the input vector theta degrees clockwise
-    vector3 = rotate_z(vector2, theta)
-    # Reverse the earlier x axis rotation
-    vector4 = rotate_x(vector3, -x_angle)
-    # Reverse the initial z axis rotation
-    vector5 = rotate_z(vector4, -z_angle)
-    return vector5
+    # Rotate theta degrees clockwise around the slope_normal_vector
+    theta_rad = radians(theta)
+    return cos(theta_rad) * vector + np.matmul(sin(theta_rad) * slope_normal_vector, vector)
 # vector-utilities ends here
