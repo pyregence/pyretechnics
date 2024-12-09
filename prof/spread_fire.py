c194caed39df5656cdc6be1a3069dcdcfd782033
Ubuntu <ubuntu@ip-172-31-45-92.eu-north-1.compute.internal>
2024-12-09 12:44:20 +0000

    New crowning-inducing inputs in benchmark.

--- prof/spread_fire.py
+++ prof/spread_fire.py
@@ -10,8 +10,8 @@ import time
 
 cube_shape = (
     240, # bands: 10 days @ 1 hour/band
-    100, # rows:  3 km @ 30 meters/row
-    100, # cols:  3 km @ 30 meters/col
+    500, # rows:  3 km @ 30 meters/row
+    500, # cols:  3 km @ 30 meters/col
 )
 
 grid_shape = cube_shape[1:]
@@ -40,19 +40,20 @@ def arr3d(value):
 space_time_cubes = {
     "slope"                        : SpaceTimeCube(cube_shape, arr2d(0.8)),   # rise/run
     "aspect"                       : SpaceTimeCube(cube_shape, arr2d(225.0)), # degrees clockwise from North
-    "fuel_model"                   : SpaceTimeCube(cube_shape, arr2d(101)),   # integer index in fm.fuel_model_table
-    "canopy_cover"                 : SpaceTimeCube(cube_shape, arr2d(0.6)),   # 0-1
-    "canopy_height"                : SpaceTimeCube(cube_shape, arr2d(30.0)),  # m
-    "canopy_base_height"           : SpaceTimeCube(cube_shape, arr2d(3.0)),   # m
+    # Fuel model 185: (TL5) high-load conifer litter
+    "fuel_model"                   : SpaceTimeCube(cube_shape, arr2d(185)),   # integer index in fm.fuel_model_table
+    "canopy_cover"                 : SpaceTimeCube(cube_shape, arr2d(0.7)),   # 0-1
+    "canopy_height"                : SpaceTimeCube(cube_shape, arr2d(10.0)),  # m
+    "canopy_base_height"           : SpaceTimeCube(cube_shape, arr2d(0.5)),   # m
     "canopy_bulk_density"          : SpaceTimeCube(cube_shape, arr2d(0.3)),   # kg/m^3
-    "wind_speed_10m"               : SpaceTimeCube(cube_shape, arr3d(30.0)),  # km/hr
+    "wind_speed_10m"               : SpaceTimeCube(cube_shape, arr3d(10.0)),  # km/hr
     "upwind_direction"             : SpaceTimeCube(cube_shape, arr3d(180.0)), # degrees clockwise from North
-    "fuel_moisture_dead_1hr"       : SpaceTimeCube(cube_shape, arr3d(0.05)),  # kg moisture/kg ovendry weight
-    "fuel_moisture_dead_10hr"      : SpaceTimeCube(cube_shape, arr3d(0.10)),  # kg moisture/kg ovendry weight
-    "fuel_moisture_dead_100hr"     : SpaceTimeCube(cube_shape, arr3d(0.15)),  # kg moisture/kg ovendry weight
+    "fuel_moisture_dead_1hr"       : SpaceTimeCube(cube_shape, arr3d(0.03)),  # kg moisture/kg ovendry weight
+    "fuel_moisture_dead_10hr"      : SpaceTimeCube(cube_shape, arr3d(0.04)),  # kg moisture/kg ovendry weight
+    "fuel_moisture_dead_100hr"     : SpaceTimeCube(cube_shape, arr3d(0.05)),  # kg moisture/kg ovendry weight
     "fuel_moisture_live_herbaceous": SpaceTimeCube(cube_shape, arr3d(0.90)),  # kg moisture/kg ovendry weight
     "fuel_moisture_live_woody"     : SpaceTimeCube(cube_shape, arr3d(0.60)),  # kg moisture/kg ovendry weight
-    "foliar_moisture"              : SpaceTimeCube(cube_shape, arr3d(0.90)),  # kg moisture/kg ovendry weight
+    "foliar_moisture"              : SpaceTimeCube(cube_shape, arr3d(0.70)),  # kg moisture/kg ovendry weight
     "fuel_spread_adjustment"       : SpaceTimeCube(cube_shape, arr2d(1.0)),   # float >= 0.0 (Optional: defaults to 1.0)
     "weather_spread_adjustment"    : SpaceTimeCube(cube_shape, arr3d(1.0)),   # float >= 0.0 (Optional: defaults to 1.0)
 }
@@ -79,7 +80,8 @@ output_matrices = {
 start_time = 2070  # minutes
 
 # 8 hours
-max_duration = 480 # minutes
+#max_duration = 480 # minutes
+max_duration = 60*2 # minutes
 
 # Burn initially ignited cell into the phi matrix by setting it to -1.0
 output_matrices["phi"][50,50] = -1.0
