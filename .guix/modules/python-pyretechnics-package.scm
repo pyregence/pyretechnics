65f01cf65b89f6924ad65156727de7675090fb46
Gary Johnson <lambdatronic@disroot.org>
2025-01-13 10:27:17 -0500

    Removed dependency on lambdatronic guix channel by adding python-snakeviz package definition to guix.scm

--- .guix/modules/python-pyretechnics-package.scm
+++ .guix/modules/python-pyretechnics-package.scm
@@ -1,5 +1,6 @@
 (define-module (python-pyretechnics-package)
-  #:use-module ((gnu packages base)            #:select (coreutils which))
+  #:use-module ((gnu packages base)            #:select (coreutils
+                                                         which))
   #:use-module ((gnu packages bash)            #:select (bash))
   #:use-module ((gnu packages check)           #:select (python-pytest))
   #:use-module ((gnu packages emacs)           #:select (emacs-minimal))
@@ -7,22 +8,63 @@
   #:use-module ((gnu packages geo)             #:select (gdal))
   #:use-module ((gnu packages less)            #:select (less))
   #:use-module ((gnu packages python)          #:select (python-wrapper))
-  #:use-module ((gnu packages python-build)    #:select (python-hatchling))
-  #:use-module ((gnu packages python-xyz)      #:select (python-numpy python-rasterio python-matplotlib python-sortedcontainers python-cython-3 python-twine))
+  #:use-module ((gnu packages python-build)    #:select (python-hatchling
+                                                         python-setuptools
+                                                         python-wheel))
+  #:use-module ((gnu packages python-web)      #:select (python-tornado))
+  #:use-module ((gnu packages python-xyz)      #:select (python-numpy
+                                                         python-rasterio
+                                                         python-matplotlib
+                                                         python-sortedcontainers
+                                                         python-cython-3
+                                                         python-twine))
   #:use-module ((gnu packages ssh)             #:select (openssh))
   #:use-module ((gnu packages version-control) #:select (git))
+  #:use-module ((guix build-system python)     #:select (pypi-uri))
   #:use-module ((guix build-system pyproject)  #:select (pyproject-build-system))
+  #:use-module ((guix download)                #:select (url-fetch))
   #:use-module ((guix gexp)                    #:select (local-file))
   #:use-module ((guix git-download)            #:select (git-predicate))
-  #:use-module ((guix licenses)                #:select (epl2.0))
-  #:use-module ((guix packages)                #:select (package))
-  #:use-module ((guix utils)                   #:select (current-source-directory))
-  #:use-module ((lambdatronic python-packages) #:select (python-snakeviz)))
+  #:use-module ((guix licenses)                #:select (epl2.0 bsd-3))
+  #:use-module ((guix packages)                #:select (package origin base32))
+  #:use-module ((guix utils)                   #:select (current-source-directory)))
+
+;;============================================================
+;; Utility Functions
+;;============================================================
 
 (define vcs-file?
   ;; Return true if the given file is under version control.
   (or (git-predicate (dirname (dirname (current-source-directory))))
       (const #t)))
+
+;;============================================================
+;; Dependency Packages
+;;============================================================
+
+(define-public python-snakeviz
+  (package
+   (name "python-snakeviz")
+   (version "2.2.2")
+   (source
+    (origin
+     (method url-fetch)
+     (uri (pypi-uri "snakeviz" version))
+     (sha256
+      (base32 "10vx7b0rn3gams2qk0dj5xkbiavh8x13hykm2kzk581lirpqq0h8"))))
+   (build-system pyproject-build-system)
+   (arguments `(#:tests? #f))
+   (propagated-inputs (list python-tornado))
+   (native-inputs (list python-setuptools python-wheel))
+   (home-page "https://jiffyclub.github.io/snakeviz/")
+   (synopsis "A web-based viewer for Python profiler output")
+   (description
+    "This package provides a web-based viewer for Python profiler output.")
+   (license bsd-3)))
+
+;;============================================================
+;; The Pyretechnics Package
+;;============================================================
 
 (define-public python-pyretechnics
   (package
--- channels.scm
+++ channels.scm
@@ -8,10 +8,4 @@
         (make-channel-introduction
          "9edb3f66fd807b096b48283debdcddccfea34bad"
          (openpgp-fingerprint
-          "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
-      (channel
-       (name 'lambdatronic)
-       (url "https://git.sr.ht/~lambdatronic/guix-packages")
-       (branch "main")
-       (commit
-        "4dc78d831896d5623938de38eca368cd9fa6c9a9")))
+          "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))))
