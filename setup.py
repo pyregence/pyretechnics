9c5d08b04f239131884c9b1acc188647baca0e19
Gary Johnson <lambdatronic@disroot.org>
2025-03-06 10:35:02 -0500

    Take control of gcc compiler options in setup.py

--- setup.py
+++ setup.py
@@ -1,24 +1,45 @@
 import os
-from setuptools import setup
-from Cython.Build import cythonize
 import numpy
+from setuptools import Extension, setup
+from setuptools.command.build_ext import build_ext
+from Cython.Build import cythonize
 
 profile_cython = (os.getenv("PROFILE_CYTHON") == "1")
 
-setup(
-    name="pyretechnics",
-    include_dirs=[numpy.get_include()],
-    ext_modules=cythonize(
-        "src/pyretechnics/*.py", # module list
-        exclude="src/pyretechnics/py_types.py",
-        annotate=True,
-        compiler_directives={
-            "language_level": "3",
-            "profile": profile_cython,
-            "initializedcheck": False,
-            "cdivision": True,
-            "wraparound": False,
-            "boundscheck": False,
-        },
-    ),
-)
+class custom_build_ext(build_ext):
+    def build_extensions(self):
+        # Override the compiler executables. Importantly, this
+        # removes the "default" compiler flags that would
+        # otherwise get passed on to to the compiler, i.e.,
+        # distutils.sysconfig.get_var("CFLAGS").
+        self.compiler.set_executable("compiler_so", "gcc")
+        build_ext.build_extensions(self)
+
+extensions = [
+    Extension("*",
+              ["src/pyretechnics/*.py"],
+              include_dirs=[numpy.get_include()],
+              extra_compile_args=["-Wno-unused-result",
+                                  "-Wsign-compare",
+                                  "-DNDEBUG",
+                                  "-g",
+                                  "-fwrapv",
+                                  "-O3",
+                                  "-Wall",
+                                  "-fno-semantic-interposition",
+                                  "-fPIC"])
+]
+
+setup(name="pyretechnics",
+      cmdclass={"build_ext": custom_build_ext},
+      ext_modules=cythonize(extensions,
+                            exclude="src/pyretechnics/py_types.py",
+                            annotate=True,
+                            compiler_directives={
+                                "language_level": "3",
+                                "profile": profile_cython,
+                                "initializedcheck": False,
+                                "cdivision": True,
+                                "wraparound": False,
+                                "boundscheck": False,
+                            }))
